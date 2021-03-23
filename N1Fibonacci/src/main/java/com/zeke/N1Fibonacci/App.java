02a060573623fba846f64510fd576c3b15069319
Foltut Daniel <danfoltut@gmail.com>
2021-03-23 18:12:44 +0200

    First Program

---
+++ N1Fibonacci/src/main/java/com/zeke/N1Fibonacci/App.java
@@ -0,0 +1,13 @@
+package com.zeke.N1Fibonacci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
